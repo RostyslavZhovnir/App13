47fb821bf23b57d7a9ccc1e7173450eb5e0a61db
RostyslavZhovnir <zhovnir.info@gmail.com>
2018-12-22 23:12:59 -0600

    Add project files.

---
+++ App13/App13/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App13
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            readyForPickup.Clicked+=ReadyForPickup_Clicked;
+            offline.Clicked+=Offline_Clicked;
+        }
+
+        private async void Offline_Clicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Вы Offline", "Спасибо за огромный труд! Поскорее возвращайтесь !!", "Подтвердить");
+        }
+
+        private async void ReadyForPickup_Clicked(object sender, EventArgs e)
+        {
+           //await DisplayAlert("Запрос принят", "Ожидайте ответ от диспетчера", "Подтвердить");
+           await DisplayAlert("Вы Online", "Следите за уведомлениями с грузами вокруг вас, удачной работы !!", "Подтвердить");
+        }
+    }
+}
