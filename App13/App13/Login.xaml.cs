de8eab73f09368bc0e9a99a09a90140db643f0e3
RostyslavZhovnir <zhovnir.info@gmail.com>
2018-12-24 17:49:40 -0600

    login

--- App13/App13/Login.xaml.cs
+++ App13/App13/Login.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,13 +19,39 @@ namespace App13
 			InitializeComponent ();
 
 			login.Clicked+=login_Clicked;
+            loginfail.IsVisible=false;
 
 
-		}
+        }
 
 		private async void login_Clicked(object sender, EventArgs e)
 		{
-			await Navigation.PushAsync(new MainPage());
+			
+
+			HttpClient client = new HttpClient();
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync("http://192.168.0.12:45455/api/users1");
+                if (response.StatusCode==HttpStatusCode.OK)
+                {
+                    HttpContent responseContent = response.Content;
+                    var json = await responseContent.ReadAsStringAsync();
+                    await Navigation.PushAsync(new MainPage());
+                }
+
+                else
+                {
+                    loginfail.IsVisible=true;
+                }
+            }
+            catch (Exception )
+            {
+                loginfail.IsVisible=true;
+                loginfail.Text="Проблемы с соединением. Перезапустите приложение ";
+            }
+		
+
+
 		}
 	}
 }
