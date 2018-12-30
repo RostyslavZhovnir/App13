42a903c8031f87e6a3c1f4339026e4f7cfbcb1ce
RostyslavZhovnir <zhovnir.info@gmail.com>
2018-12-30 00:14:03 -0600

    css

--- App13/App13/Notification.xaml.cs
+++ App13/App13/Notification.xaml.cs
@@ -22,7 +22,8 @@ namespace App13
 		private string _pass;
 		private string _secKey;
 		private string _loadID;
-	   
+        string froms;
+        string tos;
 		public Notification (string msg, string username, string userpass, string userkey, string loadid)
 		{
 			_username=username;
@@ -30,11 +31,28 @@ namespace App13
 			_secKey=userkey;
 			_loadID=loadid;
 			InitializeComponent ();
-			message.Text=msg;
-			submitbid.Clicked+=_submit;
+		   submitbid.Clicked+=_submit;
 			cancel.Clicked+=_cancel;
+			direction.Clicked+=_direction;
 
-			
+			var result = msg.Split("%%".ToCharArray());
+			milestopickup.Text = "Миль до загрузки: "+result[0];
+			from.Text ="From: "+result[2];
+			to.Text ="To: "+result[4];
+			froms = result[2];
+			tos = result[4];
+			weight.Text ="Вес/кольчество: "+result[6];
+		  //totalmiles.Text ="Всего миль: "+result[8];
+			pickupdate.Text="Дата загрузки: "+result[10];
+			deliverydate.Text="Дата доставки: "+result[12];
+
+
+		}
+
+		private void _direction(object sender, EventArgs e)
+		{
+			var uri = new Uri("https://www.google.com/maps/dir/?api=1&origin="+froms+"&destination="+tos);
+			Device.OpenUri(uri);
 		}
 
 		private void _cancel(object sender, EventArgs e)
@@ -54,7 +72,7 @@ namespace App13
 				var json = JsonConvert.SerializeObject(makebid);
 				var resp = new StringContent(json, Encoding.UTF8, "application/json");
 				var response = client.PutAsync(url, resp).Result;
-                
+				
 
 				if (response.StatusCode==HttpStatusCode.OK)
 				{
